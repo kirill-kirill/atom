2e3f682f694bfa451654ff269d5b56263e4c12c0
Kirill-Kirill <cognitive.kiri2l@gmail.com>
2017-11-08 12:09:31 +0300

    Fixed all bugs and added new

---
+++ lecture01/src/main/java/ru.atom/MyFixedFile1.java
@@ -0,0 +1,8 @@
+package ru.atom;
+
+public class MyFixedFile1 {
+
+    public static void main(String[] args) {
+        System.out.println("Fixed all bugs");
+    }
+}
---
+++ lecture01/src/main/java/ru.atom/MyFixedFile2.java
@@ -0,0 +1,8 @@
+package ru.atom;
+
+public class MyFixedFile2 {
+
+    public static void main(String[] args) {
+        System.out.println("Added new bug");
+    }
+}
